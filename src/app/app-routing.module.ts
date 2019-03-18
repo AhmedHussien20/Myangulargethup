af81d3c15a46d7b63fcb116175903219a7700aba
AOT DOT NET <prograhmed@gmail.com>
2019-03-18 17:23:17 +0200

    all source code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProductComponent } from './product/product.component';
+import { MyorderComponent } from './myorder/myorder.component';
+import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
+import { AdminProductComponent } from './admin/admin-product/admin-product.component';
 
-const routes: Routes = [];
+const routes: Routes = 
+[
+  {path:'products',component:ProductComponent},
+  {path:'myorder',component:MyorderComponent},
+  {path:'admin/orders',component:AdminOrdersComponent},
+  {path:'admin/product',component:AdminProductComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { MyorderComponent } from './myorder/myorder.component';
+import { ProductComponent } from './product/product.component';
+import { AdminProductComponent } from './admin/admin-product/admin-product.component';
+import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    MyorderComponent,
+    ProductComponent,
+    AdminProductComponent,
+    AdminOrdersComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
